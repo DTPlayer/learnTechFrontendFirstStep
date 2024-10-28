3bd2741033c98e714d361b4e5dd6b3d0edaf0082
Refactor <reff06@t.me>
2024-10-29 00:32:35 +0300

    check primevue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+  import Button from 'primevue/button';
+  import IconField from 'primevue/iconfield';
+  import InputIcon from 'primevue/inputicon';
+  import {ref} from 'vue';
+
+  const value1: Ref<string> = ref('');
+  const value2: Ref<string> = ref('');
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="p-4 flex flex-row gap-4">
     <Button label="Verify" />
+    <IconField>
+      <InputIcon class="pi pi-search" />
+      <InputText v-model="value1" placeholder="Search" />
+    </IconField>
+
+    <IconField>
+      <InputText v-model="value2" variant="filled" />
+      <InputIcon class="pi pi-spin pi-spinner" />
+    </IconField>
+
   </div>
 </template>
 
